2e8096d421a4e91c5f53c0a18dc4761991092183
KingOffPajar <106890200+KingOffPajar@users.noreply.github.com>
2022-06-05 07:20:21 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -86,9 +86,7 @@ exports.notNum = (q) => {
 }
 exports.menunya = (salam, pushname, botname) =>{
 	return`Hai kak ${pushname} 👋 selamat ${salam} , 
-saya ${botname}, bot ini adalah Beta Multi-Device Whatsapp. 
-Jika kamu menemukan semacam bug atau kesalahan mohon dimaklumi dulu ya, 
-lapor owner agar segera di perbaiki 🙏      
+saya ${botname}, bot ini adalah Beta Multi-Device Whatsapp.
 `
 }
 exports.listMenu = (time, salam, pushname, prefix) => {
@@ -125,6 +123,9 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ├ ${prefix}bc [text]
 ├ ${prefix}bcgc [text]
 ├ ${prefix}nsfw [on/off]
+├ ${prefix}mute [on/off]
+├ ${prefix}banchat
+├ ${prefix}unbanchat
 ├ ${prefix}autorespond [on/off]
 ├ ${prefix}antiviewonce [on/off]
 ├ ${prefix}join [link]
@@ -132,6 +133,20 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ├ ${prefix}public [only bot]
 ├ ${prefix}del [reply pesan bot]
 ╰❒ ${prefix}setppbot [reply image]
+
+╭─❒ 「 Store Menu 」
+├ ${prefix}list
+├ ${prefix}addlist [key|respond]
+├ ${prefix}dellist [key]
+├ ${prefix}update [key|respond]
+├ ${prefix}store
+├ ${prefix}kali
+├ ${prefix}bagi
+├ ${prefix}tambah
+├ ${prefix}kurang
+├ ${prefix}kalkulator
+├ ${prefix}proses
+╰❒ ${prefix}done [surah|ayat]
 
 
 ╭─❒ 「 Database 」 
@@ -188,6 +203,7 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ╭─❒ 「 Search 」 
 ├ ${prefix}ytsearch [query]
 ├ ${prefix}wallpaper [query]
+├ ${prefix}google [query]
 ├ ${prefix}wikimedia [query]
 ├ ${prefix}hentai
 ├ ${prefix}wattpad [query]
@@ -766,7 +782,7 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ├ ${prefix}sponsbob
 ├ ${prefix}kawan_sponsbob
 ╰❒ ${prefix}tyni
-,
+
 ╭─❒ 「 Random Cewe 」
 ├ ${prefix}china 
 ├ ${prefix}indonesia 
@@ -779,19 +795,6 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 ├ ${prefix}jiso 
 ├ ${prefix}lisa  
 ╰❒ ${prefix}rose
-
-╭─❒ 「 TqTo 」 
-├ My God
-├ My Parents
-├ Fatih A.
-├ Ferdi
-├ DikaArdnt
-├ Mhankbarbar
-├ Nurutomo
-├ Rashid
-├ ZeeoneOfc
-├ Penyedia Module
-╰❒ And All Support
     `
 }
 
@@ -900,6 +903,9 @@ return`╭─❒ 「 Owner 」
 ├ ${prefix}bc [text]
 ├ ${prefix}bcgc [text]
 ├ ${prefix}nsfw [on/off]
+├ ${prefix}mute [on/off]
+├ ${prefix}banchat
+├ ${prefix}unbanchat
 ├ ${prefix}autorespond [on/off]
 ├ ${prefix}antiviewonce [on/off]
 ├ ${prefix}join [link]
@@ -972,6 +978,7 @@ exports.search = (prefix) =>{
 	return`╭─❒ 「 Search 」 
 ├ ${prefix}ytsearch [query]
 ├ ${prefix}wallpaper [query]
+├ ${prefix}google [query]
 ├ ${prefix}wikimedia [query]
 ├ ${prefix}hentai
 ├ ${prefix}wattpad [query]
@@ -1457,13 +1464,6 @@ exports.tqto = () =>{
 	return`╭─❒ 「 TqTo 」 
 ├ My God
 ├ My Parents
-├ Fatih A.
-├ Ferdi
-├ DikaArdnt
-├ Mhankbarbar
-├ Nurutomo
-├ Rashid
-├ ZeeoneOfc
 ├ Penyedia Module
 ╰❒ And All Support
 `
@@ -1701,6 +1701,24 @@ exports.islammenu = (prefix) =>{
 ╰❒ ${prefix}alquranaudio [surah|ayat]
 `
 }
+
+exports.storemenu = (prefix) =>{
+	return`╭─❒ 「 Store Menu 」
+├ ${prefix}list
+├ ${prefix}addlist [key|respond]
+├ ${prefix}dellist [key]
+├ ${prefix}update [key|respond]
+├ ${prefix}store
+├ ${prefix}kali
+├ ${prefix}bagi
+├ ${prefix}tambah
+├ ${prefix}kurang
+├ ${prefix}kalkulator
+├ ${prefix}proses
+╰❒ ${prefix}done
+`
+}
+
 
 exports.soundmenu = (prefix) =>{
 return`╭─❒ 「 Sound Menu 」
